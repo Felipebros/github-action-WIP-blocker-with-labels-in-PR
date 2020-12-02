72338137c88664c8dc5cb66effb4256a6ce6aa4f
Felipe Soares Barbosa Silveira <felipeiturama@gmail.com>
2020-12-02 20:52:54 -0300

    feat: added checking variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,28 @@
 
 set -e
 
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_EVENT_PATH" ]]; then
+  echo "Set the GITHUB_EVENT_PATH env variable."
+  exit 1
+fi
+
+echo "checking if it's a PR"
+echo $GITHUB_EVENT_PATH
+(jq -r ".pull_request.url" "$GITHUB_EVENT_PATH") || exit 78
+
+URI="https://api.github.com"
+API_HEADER="Accept: application/vnd.github.v3+json"
+AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+
 echo "Teste"
-exit 1
+return 1
