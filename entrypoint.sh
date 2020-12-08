0c7a4df472a80d448b0d2199e71c4d021abc0187
Felipe Soares Barbosa Silveira <felipeiturama@gmail.com>
2020-12-08 04:46:10 -0300

    feat: added function run_ci_when_approved

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,8 +22,6 @@ if [[ -z "$WIP_LABELS" ]]; then
   exit 1
 fi
 
-echo "checking if it's a PR"
-echo $GITHUB_EVENT_PATH
 (jq -r ".pull_request.url" "$GITHUB_EVENT_PATH") || exit 78
 
 URI="https://api.github.com"
@@ -31,6 +29,8 @@ API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
 number=$(jq -r ".pull_request.number" "$GITHUB_EVENT_PATH")
+action=$(jq -r ".action" "$GITHUB_EVENT_PATH")
+state=$(jq -r ".review.state" "$GITHUB_EVENT_PATH")
 
 check_contains_wip_label() {
   RESPONSE=$(
@@ -50,8 +50,40 @@ check_contains_wip_label() {
   fi
 }
 
+run_ci_when_approved() {
+  # https://developer.github.com/v3/pulls/reviews/#list-reviews-on-a-pull-request
+  body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${number}/reviews?per_page=100")
+  reviews=$(echo "$body" | jq -r '.[] | {state: .state} | @base64')
+
+  approvals=0
+
+  for r in $reviews; do
+    review="$(echo "$r" | base64 -d)"
+    rState=$(echo "$review" | jq -r '.state')
+
+    if [[ "$rState" == "APPROVED" ]]; then
+      approvals=$((approvals + 1))
+    fi
+
+    echo "${approvals}/${APPROVALS} approvals"
+
+    if [[ "$approvals" -ge "$APPROVALS" ]]; then
+      echo "Labeling pull request"
+
+      echo "executar o check lint se já não estiver executado"
+
+      break
+    fi
+  done
+}
+
 process() {
   check_contains_wip_label
+  if [[ "$action" == "submitted" ]] && [[ "$state" == "approved" ]]; then
+    run_ci_when_approved
+  else
+    echo "Ignoring event ${action}/${state}"
+  fi
 }
 
 process
